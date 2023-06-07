7d4c882008e2896a60b87dd15cbd4d2428ea9fe3
Lucas Adami <127154863+lucas-adami@users.noreply.github.com>
2023-06-07 17:12:58 -0300

    Add files via upload

---
+++ exercicio8.py
@@ -0,0 +1,4 @@
+base = float(input("Digite a medida da base do triangulo: "))
+altura = float(input("Digite a medida da altura do triangulo: "))
+area = (base * altura) / 2
+print(f"A área do triangulo vai ser {area:2}")
