b665749e3f07706a0b9e0160367f196580d0e170
Lucas Adami <127154863+lucas-adami@users.noreply.github.com>
2023-06-08 11:11:34 -0300

    Add files via upload

---
+++ exercicio11.py
@@ -0,0 +1,8 @@
+pes = float(input("Insira um medida em pés: "))
+polegada = pes / 12
+jardas = 3 * pes
+milha = jardas * 1760
+
+print(f"A sua medida em pes {pes} convertida para polegada ficou {polegada:2}")
+print(f"A sua medida em pes {pes} convertida para jardas ficou {jardas:2}")
+print(f"A sua medida em pes {pes} convertida para milhas ficou {milha:2}")
