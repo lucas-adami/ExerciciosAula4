1606710519fec6fba28ab9bc94e66f2114fee895
Lucas Adami <127154863+lucas-adami@users.noreply.github.com>
2023-06-08 11:11:10 -0300

    Add files via upload

---
+++ exercicio10.py
@@ -0,0 +1,8 @@
+from math import sqrt
+from math import pow
+
+digito = int(input("Insira um numero inteiro: ")) 
+
+print(f"O {digito} elevado ao quadrado é {pow(digito, 2)}")
+print(f"O {digito} elevado ao cubo é {pow(digito, 3)}")
+print(f"A raiz quadrada de {digito} é: {sqrt(digito)}")
